a7d48d6a87e20adde4767c79ce873db03f7c6c5a
Nicolas Llanos <nicollanos8799@gmail.com>
2025-08-08 19:26:28 +0200

    buenos advances sobre la animations

---
+++ js/stacking.js
@@ -0,0 +1,20 @@
+
+  const cards = document.querySelectorAll('.stack-card');
+
+  function revealCardsOnScroll() {
+    let visibleCount = 0;
+
+    cards.forEach((card, index) => {
+      const rect = card.getBoundingClientRect();
+      const windowHeight = window.innerHeight;
+
+      if (rect.top < windowHeight - 100) {
+        card.classList.add('visible');
+        card.style.zIndex = visibleCount + 1; // ahora el último que aparece tiene el z-index más alto
+        visibleCount++;
+      }
+    });
+  }
+
+  window.addEventListener('scroll', revealCardsOnScroll);
+  window.addEventListener('load', revealCardsOnScroll);
