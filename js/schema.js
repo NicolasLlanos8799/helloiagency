1c95079a73e35abac6f4dde53cad3ad3573e7c27
Nicolas Llanos <nicollanos8799@gmail.com>
2026-02-21 17:38:33 +0100

    chore: Remove pricing details from schema and UI.

--- js/schema.js
+++ js/schema.js
@@ -57,13 +57,6 @@ const schemaData = {
                     "@type": "AdministrativeArea",
                     "name": "Latinoamérica"
                 }
-            ],
-            "offers": [
-                {
-                    "@type": "Offer",
-                    "priceCurrency": "USD",
-                    "description": "Implementaciones desde 150 USD + mantenimiento mensual"
-                }
             ]
         },
         {
